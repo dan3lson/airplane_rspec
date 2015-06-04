812069de40c6adb494fb91891130663629e3ea39
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-03 20:15:05 -0400

    Unit tests complete for airplane.

--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,9 +1,23 @@
 require_relative "../../lib/airplane"
 
-describe Airplane do
+RSpec.describe Airplane do
 
-  describe do
-    
+  let(:flying_airplane) { flying_airplane = Airplane.new("flying") }
+
+  describe "#flying?" do
+
+    context "when the plane is flying" do
+
+      it "returns true" do
+        expect(flying_airplane.flying?).to eq true
+      end
+    end
+
+    context "when the plane isn\'t flying" do
+      it "returns false" do
+        expect(!flying_airplane.flying?).to eq false
+      end
+    end
   end
 
 end
